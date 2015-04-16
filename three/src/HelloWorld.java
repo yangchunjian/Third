ddc7e98b303770b1e353f1d41ca4d9675617f799
yangchunwei <1091938307@qq.com>
2015-04-17 07:56:11 +0800

    Add system.out.println("Hello World!");

--- three/src/HelloWorld.java
+++ three/src/HelloWorld.java
@@ -6,7 +6,11 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello World0!");
+		System.out.println("Hello World1!");
+		System.out.println("Hello World2!");
+		System.out.println("Hello World3!");
+		
 	}
 
 }
